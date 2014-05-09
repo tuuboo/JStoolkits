1a0f858e423a4591c1eaee5fdcc99f88f513f9be
tuuboo <tuuboo@gmail.com>
2014-05-10 02:42:46 +0800

    Update font-metrics.js

--- font-metrics.js
+++ font-metrics.js
@@ -9,15 +9,48 @@ var FontMetrics = function($, style, fontSize){
   tEl.css("top", "-9999px");
   tEl.appendTo(document.body);
   
-  var usFontWidth = [];
-  var cnFontWidth = 0;
-  for(var i=0; i<=255;i++) {
+  this.usFontWidth = [];
+  this.cnFontWidth = 0;
+  for(var i=0; i<=255; i++) {
     tEl.text(String.fromCharCode(i));
-    usFontWidth.push(tEl.width())
+    this.usFontWidth.push(tEl.width())
   }
-  tEl.text("1 1"), usFontWidth[32] = tEl.width() - 2*usFontWidth["1".charCodeAt(0)]
-  tEl.text("\u4f60"), cnFontWidth = tEl.width();
+  tEl.text("1 1"), this.usFontWidth[0x20] = tEl.width() - 2*this.usFontWidth["1".charCodeAt(0)]
+  tEl.text("\u4f60"), this.cnFontWidth = tEl.width();
   
   tEl.remove();
   tEl = null;
+  
+  this.dotdotdotWidth = this.usFontWidth[46] * 3;
+  this.maxFontWidth = this.cnFontWidth;
+  for(var i=0; i<this.usFontWidth.length; i++) {
+    if(this.usFontWidth[i]>this.maxFontWidth){
+      this.maxFontWidth = this.usFontWidth[i];
+    }
+  }
+  
+  this.width = function(str){
+    var w = 0;
+    for(var i=0; i<str.length; i++) {
+      w = w + (str.charCodeAt(i) <= 255?this.usFontWidth[str.charCodeAt(i)]:this.cnFontWidth);
+    }
+    return w;
+  };
+  
+  this.strip = function(str, maxWidth){
+    if(str.length * this.maxFontWidth < maxWidth){
+      return str;
+    }
+    var w = 0, idx = 0;
+    for(var i=0; i<str.length; i++) {
+      w = w + (str.charCodeAt(i) <= 255?this.usFontWidth[str.charCodeAt(i)]:this.cnFontWidth);
+      if(w+this.dotdotdotWidth <= maxWidth){
+        idx = i+1;
+      }
+      if(w > maxWidth){
+        return str.substr(0, idx) + "...";
+      }
+    }
+    return str;
+  }
 };
